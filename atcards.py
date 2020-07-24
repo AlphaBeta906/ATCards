969e365782c306d8f73ac3103f3cba35b44f8106
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2020-07-24 20:32:57 +0800

    Update atcards.py

--- atcards.py
+++ atcards.py
@@ -4,14 +4,3 @@ from random import randint
 import requests
 print ("ATCards 2020")
 print ("Version 1.0.0")
-test1 = requests.get("https://raw.githubusercontent.com/AlphaBeta906/ATCards/master/atcards.py")
-test1 = test1.text
-te = open("atcards.py", "r")
-tes = te.read()
-if test1 == tes:
-    print ("Its ok its same")
-else:
-    print ("Uh oh, the file has been updated. Wait five minuites if it says this again get it updated.")
-del test1, te, tes
-
-
