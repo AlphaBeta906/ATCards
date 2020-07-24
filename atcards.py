e36ab573a15e26b65feba16d356df87914780cf9
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2020-07-24 19:10:13 +0800

    Add files via upload

--- atcards.py
+++ atcards.py
@@ -4,9 +4,12 @@ from random import randint
 import requests
 print ("ATCards 2020")
 print ("Version 1.0.0")
-#test1 = requests.get("https://raw.githubusercontent.com/timswyzen/discordTCG/master/config.py")
-#print (test1.text)
-#te = open("atcards.py", "r")
-#tes = te.read()
-#print (tes)
+test1 = requests.get("https://raw.githubusercontent.com/AlphaBeta906/ATCards/master/atcards.py")
+test1 = test1.text
+te = open("atcards.py", "r")
+tes = te.read()
+if test1 != tes:
+    print ("TEST WORKS!")
+del test1, te, tes
+
 
