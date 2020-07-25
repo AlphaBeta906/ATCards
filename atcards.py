0f095b5ddea866fbc3f25c90929cd161581468b5
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2020-07-25 18:49:40 +0800

    Update atcards.py

--- atcards.py
+++ atcards.py
@@ -2,7 +2,7 @@
 from time import *
 from random import randint
 print ("ATCards 2020")
-print ("Version 1.0.1")
+print ("Version 1.0.2")
 playercards = {"Republic of Castile": ["120", "90"], "Kingdom of Leon": ["100", "110"], "Belastan": ["110", "100"]}
 cardinfo = {"Republic of Castile": ["Towering Castile: Increaces ATK and DEF for 5 moves(reacharge 2 moves)"], "Kingdom of Leon": ["Leon Blast: Increase ATK by 50 for one attack(reacharge of 6 moves)"], "Belastan": ["Polish Army: Draw 2 cards"]}
 currentcards = {}
