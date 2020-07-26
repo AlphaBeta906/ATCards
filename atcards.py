3d4b230261109a026bd03fc82f68ab2339e30dfa
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2020-07-26 12:56:12 +0800

    Add files via upload

--- atcards.py
+++ atcards.py
@@ -2,10 +2,12 @@
 from time import *
 from random import randint
 print ("ATCards 2020")
-print ("Version 1.0.2")
-playercards = {"Republic of Castile": ["120", "90"], "Kingdom of Leon": ["100", "110"], "Belastan": ["110", "100"]}
-cardinfo = {"Republic of Castile": ["Towering Castile: Increaces ATK and DEF for 5 moves(reacharge 2 moves)"], "Kingdom of Leon": ["Leon Blast: Increase ATK by 50 for one attack(reacharge of 6 moves)"], "Belastan": ["Polish Army: Draw 2 cards"]}
+print ("Version 1.0.1")
+playercards = {"Republic of Castile": ["120", "90"], "Kingdom of Leon": ["100", "110"], "Belastan": ["110", "100"], "Duchy of Koelsa": ["90", "100"], "Satlantis": ["80", "90"], "Atlantis": ["90", "90"]}
+cardinfo = {"Republic of Castile": ["Towering Castile: Increaces ATK and DEF for 5 moves(reacharge 2 moves)"], "Kingdom of Leon": ["Leon Blast: Increase ATK by 50 for one attack(reacharge of 6 moves)"], "Belastan": ["Polish Army: Draw 2 cards"], "Duchy of Koelsa": ["Power Steal: Get 1 card from you're opponent's card"], "Satlantis": ["Altantic Power: If you have Atlantis in you're deck increase ATK to 110 until Atlantis dies"], "Atlantis": ["Altantic Power: If you have Satlantis in you're deck increase ATK to 110 until Satlantis dies"]}
 currentcards = {}
 cs = input("> ")
-print (f"{cs}, ATK:{playercards[cs][0]} DEF:{playercards[cs][1]}")
-print ("/n".join(cardinfo[cs]))
+if cs in list(playercards):
+    print (f"{cs}, ATK:{playercards[cs][0]} DEF:{playercards[cs][1]}")
+    print ("/n".join(cardinfo[cs]))
+    
