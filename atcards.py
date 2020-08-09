6f82cb12c49b878ae5f3992a0e388f2f00297c67
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2020-08-09 09:52:35 +0800

    Add files via upload

--- atcards.py
+++ atcards.py
@@ -2,7 +2,7 @@
 from time import *
 from random import *
 import pickle as p
-print ("ATCards [Version 1.3.15.6]")
+print ("ATCards [Version 1.4.15.6]")
 print ("(cc) 2020 Alternate Territories Wiki")
 playercards = {"Republic of Castile": ["120", "90"], "Kingdom of Leon": ["100", "110"], "Belastan": ["110", "100"], "Duchy of Koelsa": ["90", "100"], "Satlantis": ["80", "90"], "Atlantis": ["90", "90"], "Alphadonia": ["130", "160"], "Googleplus Republic": ["100", "130"], "The New Republic of Assaria": ["70", "90"], "Olderion": ["30", "50"], "PR Asturas": ["150", "160"], "ACTO": ["200", "180"], "DigitalNewia": ["120", "130"], "New Republic of Chrome": ["50", "60"], "Saxony Kingdom": ["120", "100"], "Kingdom of Israel": ["50", "80"], "UCSO": ["200", "170"], "Anti-OK Alliance": ["130", "120"], "4th German Reich": ["70", "90"]}
 cardinfo = {"Republic of Castile": ["Towering Castile: Increaces ATK and DEF for 5 moves(reacharge 2 moves)"], "Kingdom of Leon": ["Leon Blast: Increase ATK by 50 for one attack(reacharge of 6 moves)"], "Belastan": ["Polish Army: Draw 2 cards"], "Duchy of Koelsa": ["Power Steal: Get 1 card from you're opponent's card"], "Satlantis": ["Altantic Power: If you have Atlantis in you're deck increase ATK to 110 until Atlantis dies"], "Atlantis": ["Altantic Power: If you have Satlantis in you're deck increase ATK to 110 until Satlantis dies"], "Alphadonia": ["Reign of Alpha: Increse DEF and ATK to 200 for 10 moves(used once)", "Tech Master: Draw a card"], "Googleplus Republic": ["Google Allies: If any of the Google Cards(Google Republic, New Republic of Crome, YouTube Federation and Googleplus Republic) is in you're deck, increse ATK to by 40 until card dies"], "The New Republic of Assaria": ["Asserian Power: Increase ATK to 110 for 3 moves"], "PR Asturas": ["Communist Rule: Increase ATK and DEF to 210 for 15 moves(used once)", "Tech Master: Draw a card"], "ACTO": ["Capitalist Power: Increase ATK by 20 to all of you're card's in you're deck"], "New Republic of Chrome": ["Google Allies: If any of the Google Cards(Google Republic, New Republic of Crome, YouTube Federation and Googleplus Republic) is in you're deck, increse ATK to by 40 until card dies"], "Kingdom of Israel": ["Star of David: Increase ATK by 20 for 5 moves(recharge of 10 moves)"], "UCSO": ["Communist Power: Increase ATK by 20 for 10 moves(recrage of 5 moves)"]}
@@ -18,27 +18,70 @@ epic = ["UCSO"]
 print("common/rare/ultra/epic")
 cs = input("> ").lower()
 if cs == "common" or "rare" or "ultra" or "epic":
-    for x in range(1, 6):
-        print ("====Card " + str(index) + "====")
-        if cs == "common":
+    if cs == "common" and money == 10:
+        money -= 10
+        for x in range(1, 6):
+            print ("====Card " + str(index) + "====")
             card = choice(common)
-        elif cs == "rare":
+            print (f"{card}, ATK: {playercards[card][0]} DEF: {playercards[card][1]}")
+            sleep(1)
+            print ("Special Moves:")
+            sleep(1)
+            try:
+                print ("\n".join(cardinfo[card]))
+            except KeyError:
+                print ("No special moves")
+            enter = input()
+            index += 1
+            currentcards.append(card)
+    elif cs == "rare" and money == 40:
+        money -= 40
+        for x in range(1, 6):
+            print ("====Card " + str(index) + "====")
             card = choice(rare)
-        elif cs == "ultra":
+            print (f"{card}, ATK: {playercards[card][0]} DEF: {playercards[card][1]}")
+            sleep(1)
+            print ("Special Moves:")
+            sleep(1)
+            try:
+                print ("\n".join(cardinfo[card]))
+            except KeyError:
+                print ("No special moves")
+            enter = input()
+            index += 1
+            currentcards.append(card)
+    elif cs == "ultra" and money == 100:
+        money -= 100
+        for x in range(1, 6):
+            print ("====Card " + str(index) + "====")
             card = choice(ultra)
-        else:
+            print (f"{card}, ATK: {playercards[card][0]} DEF: {playercards[card][1]}")
+            sleep(1)
+            print ("Special Moves:")
+            sleep(1)
+            try:
+                print ("\n".join(cardinfo[card]))
+            except KeyError:
+                print ("No special moves")
+            enter = input()
+            index += 1
+            currentcards.append(card)
+    elif cs == "epic" and money == 200:
+        money -= 200
+        for x in range(1, 6):
+            print ("====Card " + str(index) + "====")
             card = choice(epic)
-        print (f"{card}, ATK: {playercards[card][0]} DEF: {playercards[card][1]}")
-        sleep(1)
-        print ("Special Moves:")
-        sleep(1)
-        try:
-            print ("\n".join(cardinfo[card]))
-        except KeyError:
-            print ("No special moves")
-        enter = input()
-        index += 1
-        currentcards.append(card)
+            print (f"{card}, ATK: {playercards[card][0]} DEF: {playercards[card][1]}")
+            sleep(1)
+            print ("Special Moves:")
+            sleep(1)
+            try:
+                print ("\n".join(cardinfo[card]))
+            except KeyError:
+                print ("No special moves")
+            enter = input()
+            index += 1
+            currentcards.append(card)
 else:
     print ("No pack named '" + cs + "'")
 sleep(3)
